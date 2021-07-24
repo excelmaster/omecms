b6fb09e0c5989b75106b68cc13534803a3f53da6
betfie1@gmail.com <betfie1@gmail.com>
2021-07-23 21:40:20 -0500

    prueba scorm en local

--- ome/app/Views/test/index.php
+++ ome/app/Views/test/index.php
@@ -4,9 +4,11 @@ $inactivos = 0;
 
 $this->extend('templates/template', $site);
 $this->section('content');
-?>
+echo ('<h1>hola '.$site.'</h1>');
 
-hola
+?>
+<iframe src="<?php echo base_url('public/scorm_kids/m4/l1/1_3/index.html')  ?>" style="width:700px;height: 400px;"></iframe>
+hola.
 
 <?php
 $this->endSection();
