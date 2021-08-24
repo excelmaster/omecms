bd9b6ddd8174388488f90eb96bf79e4b4d6aa3ba
root <root@server1.mundoeducativodigital.com>
2021-08-24 12:20:38 -0500

    mejoras imagenes

--- omedev/app/Config/App.php
+++ omedev/app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
 	 *
 	 * @var string
 	 */
-	public $baseURL = 'https://form.mundoeducativodigital.com/';
+	public $baseURL = 'https://omedev.mundoeducativodigital.com/';
 
 	/**
 	 * --------------------------------------------------------------------------
