0ca00899e0489c8bb7042a8945b4b498fca1eb4b
root <root@server1.mundoeducativodigital.com>
2021-08-26 20:47:30 -0500

    implementacion nueva version

---
+++ omedev/public/scorm_teens/m1/l3/3_1/precache-manifest.c290f93b1461d7f9f7bf2398772f86d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a41114105a8335a10462d1811d0378",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "95f329e2ecbd94d45633",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "8b32906356a4acd4c172",
+    "url": "@yield('content-viewer-url')/static/css/main.0cb2cff8.chunk.css"
+  },
+  {
+    "revision": "95f329e2ecbd94d45633",
+    "url": "@yield('content-viewer-url')/static/js/2.5d0c7356.chunk.js"
+  },
+  {
+    "revision": "f6681c7661458c30d69c95815d12061b",
+    "url": "@yield('content-viewer-url')/static/js/2.5d0c7356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b32906356a4acd4c172",
+    "url": "@yield('content-viewer-url')/static/js/main.81f6cb34.chunk.js"
+  },
+  {
+    "revision": "05c91422a8ff984def11",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.779b656d.js"
+  }
+]);
---
+++ omedev/public/scorm_teens/m1/l8/8_1/precache-manifest.b2e6945a987f70e9404bda6d4346ec91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cecbf0edb04e238cdc5774114d42276c",
+    "url": "@yield('content-viewer-url')/index.html"
+  },
+  {
+    "revision": "95f329e2ecbd94d45633",
+    "url": "@yield('content-viewer-url')/static/css/2.37be29a2.chunk.css"
+  },
+  {
+    "revision": "c65d72778066edfa365e",
+    "url": "@yield('content-viewer-url')/static/css/main.0cb2cff8.chunk.css"
+  },
+  {
+    "revision": "95f329e2ecbd94d45633",
+    "url": "@yield('content-viewer-url')/static/js/2.5d0c7356.chunk.js"
+  },
+  {
+    "revision": "f6681c7661458c30d69c95815d12061b",
+    "url": "@yield('content-viewer-url')/static/js/2.5d0c7356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c65d72778066edfa365e",
+    "url": "@yield('content-viewer-url')/static/js/main.930d20d0.chunk.js"
+  },
+  {
+    "revision": "05c91422a8ff984def11",
+    "url": "@yield('content-viewer-url')/static/js/runtime-main.779b656d.js"
+  }
+]);
